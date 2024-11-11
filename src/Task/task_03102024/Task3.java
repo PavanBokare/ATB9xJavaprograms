b3e9e0fdd62aa65e6159154c1e817f3da69f80d3
Pavan <pavanbokare04@gmail.com>
2024-11-11 16:53:07 +0530

    All the files are added

---
+++ src/Task/task_03102024/Task3.java
@@ -0,0 +1,11 @@
+package Task.task_03102024;
+
+public class Task3 {
+    public static void main(String[] args) {
+        //Create a calculator
+        System.out.println(10+5); //add
+        System.out.println(10-5); //sub
+        System.out.println(10*5); //mul
+        System.out.println(10/5); //Div
+    }
+}
